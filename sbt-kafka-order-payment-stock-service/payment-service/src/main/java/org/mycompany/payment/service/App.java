accc284ff3d9325bc6a3f46e5d6c22b607fa472f
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-06 20:18:20 +0530

    Initial version of kafa-order-payment-stock micro-service

---
+++ sbt-kafka-order-payment-stock-service/payment-service/src/main/java/org/mycompany/payment/service/App.java
@@ -0,0 +1,11 @@
+package org.mycompany.payment.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
