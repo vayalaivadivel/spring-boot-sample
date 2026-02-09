85f21014c669ad90bc9b59c24e4bb4a6db505fdd
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-09 18:00:38 +0530

    Deploy.yml file changes for CD/CD

--- sbt-kafka-order-payment-stock-service/core-common/src/main/java/org/mycompany/core/common/Order.java
+++ sbt-kafka-order-payment-stock-service/core-common/src/main/java/org/mycompany/core/common/Order.java
@@ -13,12 +13,10 @@ public record Order(
     public Order(Long id, Long customerId, Long productId, String status) {
         this(id, customerId, productId, 0, 0, status, null);
     }
-
     // Constructor 2: Defaults status to "NEW" and source to null
     public Order(Long id, Long customerId, Long productId, int productCount, int price) {
         this(id, customerId, productId, productCount, price, "NEW", null);
     }
-
     public Order(Long id, Long customerId, Long productId, int productCount, int price, String status) {
         this(id, customerId, productId, productCount, price, status, null);
     }
