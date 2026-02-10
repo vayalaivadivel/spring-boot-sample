7e183032559dfb0d7c01a7d915c855c5d087517c
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-10 11:39:10 +0530

    Order-service deployement

--- sbt-kafka-order-payment-stock-service/core-common/src/main/java/org/mycompany/core/common/Order.java
+++ sbt-kafka-order-payment-stock-service/core-common/src/main/java/org/mycompany/core/common/Order.java
@@ -1,4 +1,5 @@
 package org.mycompany.core.common;
+
 public record Order(
         Long id,
         Long customerId,
@@ -12,13 +13,16 @@ public record Order(
     public Order(Long id, Long customerId, Long productId, String status) {
         this(id, customerId, productId, 0, 0, status, null);
     }
+
     // Constructor 2: Defaults status to "NEW" and source to null
     public Order(Long id, Long customerId, Long productId, int productCount, int price) {
         this(id, customerId, productId, productCount, price, "NEW", null);
     }
+
     public Order(Long id, Long customerId, Long productId, int productCount, int price, String status) {
         this(id, customerId, productId, productCount, price, status, null);
     }
+
     // This for whole controller
     public Order {
     }
