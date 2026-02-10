20c2f33d176fa77bb4ce36c967680c9a6d26e1e4
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-10 21:19:08 +0530

    This is the test for Krishna

--- sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/health/KafkaHealthIndicator.java
+++ sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/health/KafkaHealthIndicator.java
@@ -1,11 +1,14 @@
 package org.mycompany.order.health;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListTopicsOptions;
+import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
 public class KafkaHealthIndicator extends AbstractHealthIndicator {
@@ -18,14 +21,29 @@ public class KafkaHealthIndicator extends AbstractHealthIndicator {
     @Override
     protected void doHealthCheck(Health.Builder builder) {
         try {
-            var clusterId = adminClient.describeCluster().clusterId().get(3, TimeUnit.SECONDS);
-            var nodeCount = adminClient.describeCluster().nodes().get(3, TimeUnit.SECONDS).size();
+            // Cluster info
+            var cluster = adminClient.describeCluster();
+            String clusterId = cluster.clusterId().get(3, TimeUnit.SECONDS);
+            int nodeCount = cluster.nodes().get(3, TimeUnit.SECONDS).size();
+
+            // Topics
+            ListTopicsOptions options = new ListTopicsOptions();
+            options.listInternal(false); // ignore internal topics
+            ListTopicsResult topicsResult = adminClient.listTopics(options);
+
+            var topics = topicsResult.names().get(5, TimeUnit.SECONDS)
+                    .stream()
+                    .sorted()
+                    .collect(Collectors.toList());
 
             builder.up()
                     .withDetail("clusterId", clusterId)
-                    .withDetail("nodeCount", nodeCount);
+                    .withDetail("nodeCount", nodeCount)
+                    .withDetail("topics", topics);
+
         } catch (Exception e) {
-            builder.down(e);
+            builder.down(e)
+                    .withDetail("reason", e.getClass().getSimpleName() + ": " + e.getMessage());
         }
     }
 }
