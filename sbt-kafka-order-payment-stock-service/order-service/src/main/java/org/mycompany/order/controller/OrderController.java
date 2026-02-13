00877596537ffc012ee94e460a82e5e06aad2e03
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-13 20:01:01 +0530

    Order service changes

--- sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/controller/OrderController.java
+++ sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/controller/OrderController.java
@@ -52,7 +52,7 @@ public class OrderController {
 
     @PostMapping("/generate")
     public boolean create() {
-        LOG.info("Creating orders with random details");
+        LOG.info("Creating orders");
         orderGeneratorService.generate();
         return true;
     }
