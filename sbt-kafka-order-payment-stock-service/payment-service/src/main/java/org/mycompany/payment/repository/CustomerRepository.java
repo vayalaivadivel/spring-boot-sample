969a0fa902ea3e56fbab39ffc26b6939184e9afb
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-13 20:18:12 +0530

    Removed some of the services

--- sbt-kafka-order-payment-stock-service/payment-service/src/main/java/org/mycompany/payment/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.mycompany.payment.repository;
-
-import org.mycompany.payment.entity.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
