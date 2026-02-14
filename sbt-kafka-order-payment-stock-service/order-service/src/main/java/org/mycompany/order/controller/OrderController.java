4ed12cf735f103b1416db8fb2f38bbf5d3a3de4b
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-14 14:37:30 +0530

    This Ravula test

--- sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/controller/OrderController.java
+++ sbt-kafka-order-payment-stock-service/order-service/src/main/java/org/mycompany/order/controller/OrderController.java
@@ -46,7 +46,7 @@ public class OrderController {
                 order.source()
         );
         template.send("orders", updatedOrder.id(), updatedOrder);
-        LOG.info("Order sent: {}", updatedOrder);
+        LOG.info("Order has beensent: {}", updatedOrder);
         return updatedOrder;
     }
 
