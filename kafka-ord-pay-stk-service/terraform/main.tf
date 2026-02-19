3fee2da22ffbbe7464ebdee76dfc18eb28bb3f56
Vadivel M <vadivel.m@collinsongroup.com>
2026-02-20 01:26:55 +0530

    RDS Access

--- kafka-ord-pay-stk-service/terraform/main.tf
+++ kafka-ord-pay-stk-service/terraform/main.tf
@@ -225,7 +225,11 @@ resource "aws_security_group" "rds_sg" {
 ############################
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name = "rds-subnet-group"
-  subnet_ids = [aws_subnet.public.id]
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
   tags = { Name = "rds-subnet-group" }
 }
 
